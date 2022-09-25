4c742ffffbfa7c51bad4d5f4d5ed72ac3b58ecb6
Rohan Khan <rohankhan1997@outlook.com>
2022-09-25 19:53:07 +0200

    Create IFBarrier.scala

---
+++ src/main/scala/IFBarrier.scala
@@ -0,0 +1,23 @@
+package FiveStage
+import chisel3._
+import chisel3.experimental.MultiIOModule
+
+class InstructionFetchBarrier extends MultiIOModule {
+
+	val io = IO(
+    new Bundle {
+      val PC_In = Input(UInt())
+      val instruction_In = Input(new Instruction)
+
+      val PC_Out = Output(UInt())
+      val instruction_Out = Output(new Instruction)
+    })
+
+  val PC   = RegInit(0.U(32.W))
+
+  io.PC_In := PC
+
+  io.instruction_Out := io.instruction_In
+  io.PC_Out := PC
+
+}
