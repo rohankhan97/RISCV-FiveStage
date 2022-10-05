6fdd97e2b8147279599a131c5e49c9f4de1560ae
Rohan Khan <rohankhan1997@outlook.com>
2022-10-05 23:19:11 +0200

    Update EX.scala

--- src/main/scala/EX.scala
+++ src/main/scala/EX.scala
@@ -71,7 +71,7 @@ class Execute extends MultiIOModule {
     ALUOps.AND      -> (op1 & op2),
     ALUOps.OR       -> (op1 | op2),
     ALUOps.XOR      -> (op1 ^ op2),
-    ALUOps.SLT      -> (op1 < op2),
+    ALUOps.SLT      -> ((op1 < op2).asUInt).asSInt,
     ALUOps.SLTU     -> (op1.asUInt < op2.asUInt).asSInt,
     ALUOps.SLL      -> (op1.asUInt << op2(4, 0).asUInt).asSInt,
     ALUOps.SRL      -> (op1.asUInt >> op2(4, 0).asUInt).asSInt,
